f2ab8c0749b82da9a6562981431a2bc15a5950ed
zrw <1763481644@qq.com>
2022-08-04 22:37:19 +0800

    封装树形结构组件

---
+++ src/views/departments/components/tree-tool.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-row class="custom-tree-node" style="height: 40px;width: 100%">
+    <!-- 左边部门 -->
+    <el-col>
+      <span>{{ treeNode.name }}</span>
+    </el-col>
+    <!-- 右边负责人 -->
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeNode.manager }}</el-col>
+        <el-col>
+          <el-dropdown>
+            <span class="el-dropdown-link">
+              操作<i class="el-icon-arrow-down el-icon--right" />
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="isRoot">删除部门</el-dropdown-item></el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {
+    treeNode: {
+      type: Object,
+      required: true
+    },
+    isRoot: {
+      type: Boolean,
+      default: true
+
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+ .custom-tree-node {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    padding-right: 8px;
+  }
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,55 +2,11 @@
   <div class="dashboard-container">
     <div class="app-container">
       <el-card class="tree-card">
-        <!-- 用一个行列布局 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height:40px">
-          <!-- 第一列部分 -->
-          <el-col :span="12">
-            <span>盛丰物流集团有限公司</span></el-col>
-          <!-- 第二列部分 -->
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <el-dropdown>
-                  <span class="el-dropdown-link">
-                    操作<i class="el-icon-arrow-down el-icon--right" />
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>黄金糕</el-dropdown-item>
-                    <el-dropdown-item>狮子头</el-dropdown-item>
-                    <el-dropdown-item>螺蛳粉</el-dropdown-item>
-                    <el-dropdown-item disabled>双皮奶</el-dropdown-item>
-                    <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <!-- 头部 -->
+        <TreeTool :tree-node="company" :is-root="false" />
         <!-- 内容 -->
         <el-tree :data="departs" :props="defaultProps">
-          <span slot-scope="{ data }" class="custom-tree-node">
-            <!-- 左边部门 -->
-            <span>{{ data.name }}</span>
-            <!-- 右边负责人 -->
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <el-dropdown>
-                    <span class="el-dropdown-link">
-                      操作<i class="el-icon-arrow-down el-icon--right" />
-                    </span>
-                    <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>黄金糕</el-dropdown-item>
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-            </el-col>
-
-          </span>
+          <TreeTool slot-scope="{ data }" :tree-node="data" />
         </el-tree>
       </el-card>
     </div>
@@ -58,7 +14,11 @@
 </template>
 
 <script>
+import TreeTool from './components/tree-tool.vue'
 export default {
+  components: {
+    TreeTool
+  },
   data() {
     return {
       departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
@@ -67,6 +27,10 @@ export default {
       defaultProps: {
         children: 'children',
         label: 'name'
+      },
+      company: {
+        name: '盛丰物流集团有限公司',
+        manager: '负责人'
       }
     }
   }
@@ -78,12 +42,4 @@ export default {
    padding: 30px  140px;
   font-size:14px;
 }
- .custom-tree-node {
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-size: 14px;
-    padding-right: 8px;
-  }
 </style>
