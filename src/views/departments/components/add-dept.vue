5f08b802b27ca77cc13df90bb8ba7ca801f8b16a
zrw <1763481644@qq.com>
2022-08-06 18:52:00 +0800

    完成新增部门的规则校验

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -1,20 +1,20 @@
 <template>
   <el-dialog title="新增部门" :visible.sync="dialogFormVisible">
-    <el-form :model="form">
-      <el-form-item label="部门名称" :label-width="formLabelWidth">
+    <el-form :model="form" :rules="rules">
+      <el-form-item label="部门名称" :label-width="formLabelWidth" prop="name">
         <el-input v-model="form.name" autocomplete="off" style="width: 80%;" placeholder="1-50字符" />
       </el-form-item>
-      <el-form-item label="部门编码" :label-width="formLabelWidth">
-        <el-input v-model="form.name" style="width: 80%;" autocomplete="off" placeholder="1-50字符" />
+      <el-form-item label="部门编码" :label-width="formLabelWidth" prop="code">
+        <el-input v-model="form.code" style="width: 80%;" autocomplete="off" placeholder="1-50字符" />
       </el-form-item>
-      <el-form-item label="部门负责人" :label-width="formLabelWidth">
-        <el-select v-model="form.region" placeholder="请选择" style="width: 80%;">
+      <el-form-item label="部门负责人" :label-width="formLabelWidth" prop="manager">
+        <el-select v-model="form.manager" placeholder="请选择" style="width: 80%;">
           <el-option label="区域一" value="shanghai" />
           <el-option label="区域二" value="beijing" />
         </el-select>
       </el-form-item>
-      <el-form-item label="部门介绍" :label-width="formLabelWidth">
-        <el-input v-model="form.name" type="textarea" style="width: 80%;" placeholder="1-300字符" />
+      <el-form-item label="部门介绍" :label-width="formLabelWidth" prop="introduce">
+        <el-input v-model="form.introduce" type="textarea" style="width: 80%;" placeholder="1-300字符" />
       </el-form-item>
 
     </el-form>
@@ -26,6 +26,7 @@
 </template>
 
 <script>
+import { getDepartmentData } from '@/api/department'
 export default {
   name: '',
   components: {},
@@ -34,22 +35,53 @@ export default {
     dialogFormVisible: {
       type: Boolean,
       default: false
+    },
+    treeNode: {
+      type: Object,
+      required: true
     }
   },
   data() {
+    // 检查部门名称是否重复
+    const validateRepeatName = async(rule, value, callback) => {
+      const { depts } = await getDepartmentData()
+      const isRepeat = depts.filter(item => item.pid === this.treeNode.id).some(element => element.name === value)
+      isRepeat ? callback(new Error('存在相同名称部门， 请重新输入')) : callback()
+    }
+    // 检查部门编码是否重复
+    const validateRepeatCode = async(rule, value, callback) => {
+      const { depts } = await getDepartmentData()
+      const isRepeat = depts.some(element => element.code === value)
+      isRepeat ? callback(new Error(`存在相同部门编码${value}， 请重新输入`)) : callback()
+    }
     return {
     //   dialogFormVisible: true,
       form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        name: '', // 部门名称
+        code: '', // 部门编码
+        manager: '', // 部门管理者
+        introduce: '' // 部门介绍
       },
-      formLabelWidth: '120px'
+      formLabelWidth: '120px', // 表单长度
+      rules: {
+        name: [
+          { required: true, message: '请输入部门名称', trigger: 'blur' },
+          { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' },
+          { validator: validateRepeatName, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入部门编码', trigger: 'blur' },
+          { min: 1, max: 50, message: '长度在 1 到 50 个字符', trigger: 'blur' },
+          { validator: validateRepeatCode, trigger: 'blur' }
+        ],
+        manager: [
+          { required: true, message: '请选择部门负责人', trigger: 'blur' }
+        ],
+        introduce: [
+          { required: true, message: '请输入部门介绍', trigger: 'blur' },
+          { min: 1, max: 300, message: '长度在 1 到 300 个字符', trigger: 'blur' }
+        ]
+      }
     }
   },
   computed: {},
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -9,7 +9,7 @@
           <TreeTool slot-scope="{ data }" :tree-node="data" @delTreeNode="getDepartmentDataFn" @addDapa="addDapaFn" />
         </el-tree>
         <!-- 弹窗 -->
-        <addDept :dialog-form-visible="showDialog" />
+        <addDept :dialog-form-visible="showDialog" :tree-node="node" />
       </el-card>
     </div>
   </div>
@@ -34,7 +34,8 @@ export default {
       },
       company: {
         name: '盛丰物流集团有限公司',
-        manager: '负责人'
+        manager: '负责人',
+        id: ''
       },
       showDialog: false, // 是否显示弹窗
       node: null // 获取当前的node节点
