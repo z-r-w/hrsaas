dae89e388d5553b7d64534f74869ef31f618310f
zrw <1763481644@qq.com>
2022-08-20 17:20:07 +0800

    放大缩小按钮

---
+++ src/components/ScreeFull/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <SvgIcon icon-class="fullscreen" style="width:20px; height:20px; color: #fff;" @click="changeScreen" />
+  </div>
+</template>
+
+<script>
+import screenfull from 'screenfull'
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data() {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {
+  },
+  methods: {
+    // 放大和缩小
+    changeScreen() {
+      if (!screenfull.isEnabled) {
+        this.$message.warning('当前功能不能使用')
+        return
+      }
+      screenfull.toggle()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+    display: inline-block;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,14 @@
 import PageTools from './PageTools'
 import UploadExcel from './UploadExcel'
 import ImageUpload from './ImageUpload'
+import SvgIcon from './SvgIcon'
+import ScreenFull from './ScreeFull'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools) // 注册工具栏组件
     Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     Vue.component('ImageUpload', ImageUpload) // 注册导入上传组件
+    Vue.component('SvgIcon', SvgIcon)
+    Vue.component('ScreenFull', ScreenFull)
   }
 }
